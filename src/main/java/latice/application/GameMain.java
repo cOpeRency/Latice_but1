cd1458e3ba22c3fd3f7ab77c0cb9ce419ab1ddf2
Anthony Denise <anthony.denise@etu.unilim.fr>
2022-04-13 14:22:53 +0200

    Initial commit

---
+++ src/main/java/latice/application/GameMain.java
@@ -0,0 +1,14 @@
+package latice.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
