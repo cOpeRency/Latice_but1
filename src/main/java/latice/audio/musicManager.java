e18cd585540529d8dd701d68c3fbc359f4bf1c58
Anthony Denise <anthony.denise@etu.unilim.fr>
2022-06-05 18:00:20 +0200

    add music loop

--- src/main/java/latice/audio/musicManager.java
+++ src/main/java/latice/audio/musicManager.java
@@ -4,14 +4,40 @@ import java.io.File;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 import latice.vue.GameVisual;
 
 public class musicManager {
+	private static Double loopStart = 0.0;
 
 	public static MediaPlayer getBackgroundMusic(){
 		 String musicFile = "src/main/resources/musics/"+GameVisual.getTheme()+"/music1.mp3";
          Media sound = new Media(new File(musicFile).toURI().toString());
          MediaPlayer mediaPlayer = new MediaPlayer(sound);
+         
+         
+         Double loopEnd = 0.0;
+         switch (GameVisual.getTheme()) {
+         case "pokemon":
+        	 musicManager.loopStart = 17.6;
+             loopEnd = 263.5;
+             break;
+         case "one_piece":
+        	 musicManager.loopStart = 25.415;
+             loopEnd = 111.361;
+             break;
+         case "zelda":
+        	 musicManager.loopStart = 69.1;
+             loopEnd = 183.2;
+             break;
+        	 
+         }
+         mediaPlayer.setStopTime(Duration.seconds(loopEnd));
+         mediaPlayer.setOnEndOfMedia(new Runnable() {
+               public void run() {
+            	   mediaPlayer.seek(Duration.seconds(musicManager.loopStart));
+               }
+           });
          return mediaPlayer;
 	}
 }
