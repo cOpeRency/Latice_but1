de2cc18eaac5a4867751946008020f918454b2e7
Anthony Denise <anthony.denise@etu.unilim.fr>
2022-06-05 23:07:32 +0200

    separate main menu into two menu (add SelectMenu)

--- src/main/java/latice/vue/menu/GameMenu.java
+++ src/main/java/latice/vue/menu/GameMenu.java
@@ -17,13 +17,7 @@ public class GameMenu {
 	private static AnchorPane apMenu;
 	private static ImageView imgMenu;
 	private static Button btnStart;
-	private static ComboBox<String> cbTheme;
-	private static TextField tfNameJ1;
-	private static TextField tfNameJ2;
 	private static final String IMAGE_PATH = "src/main/resources/system/mainMenu_bg.png";
-	private static final String POKEMON = "Pokémon";
-	private static final String ONE_PIECE = "One Piece";
-	private static final String ZELDA = "Zelda";
 	
 	public static Scene getSceneMenu() {
 		apMenu = new AnchorPane();
@@ -43,56 +37,13 @@ public class GameMenu {
 		btnStart.setLayoutX(480);
 		btnStart.setLayoutY(500);
 		
-		cbTheme = new ComboBox<String>();
-		cbTheme.setMinSize(280, 40);
-		cbTheme.setLayoutX(500);
-		cbTheme.setLayoutY(570);
-		cbTheme.getItems().addAll(POKEMON,ONE_PIECE,ZELDA);
-		cbTheme.setValue(POKEMON);
-		
-		tfNameJ1 = new TextField();
-		tfNameJ1.setPromptText("Nom du joueur 1");
-		tfNameJ1.setMinSize(240, 40);
-		tfNameJ1.setLayoutX(100);
-		tfNameJ1.setLayoutY(630);
-		
-		tfNameJ2 = new TextField();
-		tfNameJ2.setPromptText("Nom du joueur 2");
-		tfNameJ2.setMinSize(240, 40);
-		tfNameJ2.setLayoutX(940);
-		tfNameJ2.setLayoutY(630);
-		
 		apMenu.getChildren().add(btnStart);
-		apMenu.getChildren().add(cbTheme);
-		apMenu.getChildren().add(tfNameJ1);
-		apMenu.getChildren().add(tfNameJ2);
 		sceneMenu = new Scene(apMenu,1280,720);
 		return sceneMenu;
-	}
-
-	public static GameTheme getComboBoxTheme() {
-		switch (cbTheme.getValue()) {
-		case POKEMON:
-			return GameTheme.POKEMON;
-		case ONE_PIECE:
-			return GameTheme.ONE_PIECE;
-		case ZELDA:
-			return GameTheme.ZELDA;
-		default :
-			return GameTheme.POKEMON;
-		}
 	}
 	
 	public static Button getBtnStart() {
 		return btnStart;
-	}
-
-	public static TextField getTfNameJ1() {
-		return tfNameJ1;
-	}
-
-	public static TextField getTfNameJ2() {
-		return tfNameJ2;
 	}
 	
 }
---
+++ src/main/java/latice/vue/menu/SelectMenu.java
@@ -0,0 +1,142 @@
+package latice.vue.menu;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import latice.application.LoadingScreen;
+import latice.vue.GameTheme;
+import latice.vue.GameVisual;
+
+public class SelectMenu {
+	private static Scene sceneMenu;
+	private static AnchorPane apMenu;
+	private static ImageView imgMenu;
+	private static Button btnStart;
+	private static Label lblTheme;
+	private static Button btnPokemonTheme;
+	private static Button btnOnePieceTheme;
+	private static Button btnZeldaTheme;
+	private static Button btnDbzTheme;
+	private static TextField tfNameJ1;
+	private static TextField tfNameJ2;
+	
+	private static final String IMAGE_PATH = "src/main/resources/system/selectMenu.png";
+
+	public static Scene getSceneMenu() {
+		apMenu = new AnchorPane();
+		String urlFichier;
+		try {
+			File fichier = new File(IMAGE_PATH);
+			urlFichier = fichier.toURI().toURL().toString();
+			Image img = new Image(urlFichier,1300,731,true,true);
+			imgMenu = new ImageView(img);
+			apMenu.getChildren().add(imgMenu);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		btnStart = new Button("Start");
+		btnStart.setMinSize(320, 50);
+		btnStart.setLayoutX(480);
+		btnStart.setLayoutY(650);
+		
+		lblTheme = new Label("Theme : Pokémon");
+    	GameVisual.setTheme(GameTheme.POKEMON);
+    	lblTheme.setMinSize(320, 50);
+    	lblTheme.setAlignment(Pos.CENTER);
+    	lblTheme.setLayoutX(480);
+    	lblTheme.setLayoutY(580);
+    	lblTheme.setTextFill(javafx.scene.paint.Color.WHITE);
+    	lblTheme.setFont(Font.font(null, FontWeight.NORMAL, 35));
+		
+		btnPokemonTheme = new Button("Pokemon");
+		btnPokemonTheme.setMinSize(200, 40);
+		btnPokemonTheme.setLayoutX(75);
+		btnPokemonTheme.setLayoutY(270);
+		btnPokemonTheme.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		    @Override
+		    public void handle(MouseEvent event) {
+		    	GameVisual.setTheme(GameTheme.POKEMON);
+		    	lblTheme.setText("Theme : Pokémon");
+		    }
+		});
+		
+		btnOnePieceTheme = new Button("One Piece");
+		btnOnePieceTheme.setMinSize(200, 40);
+		btnOnePieceTheme.setLayoutX(395);
+		btnOnePieceTheme.setLayoutY(270);
+		btnOnePieceTheme.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		    @Override
+		    public void handle(MouseEvent event) {
+		    	GameVisual.setTheme(GameTheme.ONE_PIECE);
+		    	lblTheme.setText("Theme : One Piece");
+		    }
+		});
+		
+		btnZeldaTheme = new Button("The Legend of Zelda");
+		btnZeldaTheme.setMinSize(200, 40);
+		btnZeldaTheme.setLayoutX(710);
+		btnZeldaTheme.setLayoutY(270);
+		btnZeldaTheme.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		    @Override
+		    public void handle(MouseEvent event) {
+		    	GameVisual.setTheme(GameTheme.ZELDA);
+		    	lblTheme.setText("Theme : The Legend of Zelda");
+		    }
+		});
+		
+		btnDbzTheme = new Button("Dragon Ball Z");
+		btnDbzTheme.setMinSize(200, 40);
+		btnDbzTheme.setLayoutX(1025);
+		btnDbzTheme.setLayoutY(270);
+		btnDbzTheme.setDisable(true);
+		
+		tfNameJ1 = new TextField();
+		tfNameJ1.setPromptText("Nom du joueur 1");
+		tfNameJ1.setMinSize(240, 40);
+		tfNameJ1.setLayoutX(100);
+		tfNameJ1.setLayoutY(630);
+		
+		tfNameJ2 = new TextField();
+		tfNameJ2.setPromptText("Nom du joueur 2");
+		tfNameJ2.setMinSize(240, 40);
+		tfNameJ2.setLayoutX(940);
+		tfNameJ2.setLayoutY(630);
+		
+		apMenu.getChildren().add(btnStart);
+		apMenu.getChildren().add(btnPokemonTheme);
+		apMenu.getChildren().add(btnOnePieceTheme);
+		apMenu.getChildren().add(btnZeldaTheme);
+		apMenu.getChildren().add(btnDbzTheme);
+		apMenu.getChildren().add(lblTheme);
+		apMenu.getChildren().add(tfNameJ1);
+		apMenu.getChildren().add(tfNameJ2);
+		sceneMenu = new Scene(apMenu,1280,720);
+		return sceneMenu;
+	}
+	
+	public static Button getBtnStart() {
+		return btnStart;
+	}
+
+	public static TextField getTfNameJ1() {
+		return tfNameJ1;
+	}
+
+	public static TextField getTfNameJ2() {
+		return tfNameJ2;
+	}
+}
